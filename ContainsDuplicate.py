a008b639bcedc3f0d025d8e02ad96c0076748a4c
Awais Choudhary <awaischoudhary@Awaiss-MacBook-Pro.local>
2024-06-17 19:18:01 -0400

    array1

---
+++ ContainsDuplicate.py
@@ -0,0 +1,13 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        hashmap = {}
+
+        for i in range(len(nums)):
+            if nums[i] in hashmap:
+                return True
+            hashmap[nums[i]] = 0
+        
+        return False
+
+                
