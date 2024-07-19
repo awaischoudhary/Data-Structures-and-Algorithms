10de651d8c0c0b63629d65d12078bfafdbedc8ce
Awais Choudhary <awaischoudharyy@gmail.com>
2024-07-18 23:59:17 -0400

    rename

--- Linked List/MergeList.py
+++ Linked List/MergeList.py
@@ -5,8 +5,8 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        node = ListNode()
-        curr = node
+        node = ListNode() # type: ignore
+        currr = node
 
         while list1 and list2:
             if list1.val < list2.val:
@@ -23,5 +23,5 @@ class Solution:
         elif list2:
             node.next = list2
 
-        return curr.next
+        return currr.next
             
